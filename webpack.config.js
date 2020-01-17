28fc06f4864e102ef29b2d1b0b5a64f67eb66906
psfonseka <psfonseka@gmail.com>
2020-01-17 12:11:07 -0500

    change extension from .js to .jsx

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const webpack = require('webpack');
 const path = require('path');
 
 const config = {
-  entry: './client/src/index.js',
+  entry: './client/src/index.jsx',
   output: {
     path: path.resolve(__dirname, 'client/dist'),
     filename: 'bundle.js'
